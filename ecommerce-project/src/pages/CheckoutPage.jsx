60cd82977d270bcf9b066e023e45871a0fca4f40
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-07-30 10:47:17 +0430

    update links to match the router paths

--- ecommerce-project/src/pages/CheckoutPage.jsx
+++ ecommerce-project/src/pages/CheckoutPage.jsx
@@ -8,7 +8,7 @@ function CheckoutPage() {
       <div class="checkout-header">
         <div class="header-content">
           <div class="checkout-header-left-section">
-            <a href="index.html">
+            <a href="/">
               <img class="logo" src="images/logo.png" />
               <img class="mobile-logo" src="images/mobile-logo.png" />
             </a>
@@ -16,7 +16,7 @@ function CheckoutPage() {
 
           <div class="checkout-header-middle-section">
             Checkout (
-            <a class="return-to-home-link" href="index.html">
+            <a class="return-to-home-link" href="/">
               3 items
             </a>
             )
