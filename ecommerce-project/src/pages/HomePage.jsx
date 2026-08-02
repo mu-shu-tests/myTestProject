b55f51a34fc24bf05ed6ae3cffcc2481e5d3d455
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-08-02 09:46:08 +0430

    add server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -24,17 +24,17 @@ fetch("http://localhost:3000/api/products")
   });
 */
   const [products, setProducts] = useState([]);
-  const [cart,setCart] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       // console.log(response.data);
       setProducts(response.data);
     });
 
-    axios.get("http://localhost:3000/api/cart-items").then((response)=>{
+    axios.get("/api/cart-items").then((response) => {
       //console.log(response.data);
       setCart(response.data);
-    })
+    });
   }, []);
 
   return (
