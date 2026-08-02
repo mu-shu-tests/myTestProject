65b9ae2160462b8bb9e0bbeb5a2ad42a7ecfbdd8
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-08-02 09:52:58 +0430

    Lift the cart state up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -5,16 +5,26 @@ import CheckoutPage from "./pages/checkout/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
 import TrackingPage from "./pages/TrackingPage";
 import NotFound from "./pages/NotFoundPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      //console.log(response.data);
+      setCart(response.data);
+    });
+  });
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
+import { formToJSON } from "axios";
 
 export default App;
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import CheckmarkIcon from "../assets/images/icons/checkmark.png";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function HomePage() {
+function HomePage({cart}) {
   /*
 fetch("http://localhost:3000/api/products")
   .then((Response) => {
@@ -24,17 +24,14 @@ fetch("http://localhost:3000/api/products")
   });
 */
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       // console.log(response.data);
       setProducts(response.data);
     });
 
-    axios.get("/api/cart-items").then((response) => {
-      //console.log(response.data);
-      setCart(response.data);
-    });
+
   }, []);
 
   return (
