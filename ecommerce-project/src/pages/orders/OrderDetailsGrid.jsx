bb62b8a3630c76d05d15328e53410054f2c84489
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-08-06 09:51:19 +0430

    7i ex add the orderid and productId into url parameters

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -28,7 +28,7 @@ function OrderDetailsGrid({ order }) {
             </div>
 
             <div className="product-actions">
-              <Link to="/tracking">
+              <Link to={`/tracking/${order.id}/${orderProduct.product.id}`}>
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
