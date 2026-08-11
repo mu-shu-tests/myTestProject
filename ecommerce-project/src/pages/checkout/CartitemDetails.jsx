6765f5380691570a80cf97b6053b9bc4341b9736
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-08-11 09:28:13 +0430

    8f ex switch between displaying the quantity input

--- ecommerce-project/src/pages/checkout/CartitemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartitemDetails.jsx
@@ -1,6 +1,19 @@
 import formatCurrency from "../utils/money";
 import axios from "axios";
+import { useState } from "react";
 function CartitemDetails({ cartItem, loadCart }) {
+  const [update, setUpdate] = useState(false);
+
+  const showUpdateInput = () => {
+    if (update) {
+      setUpdate(false);
+      console.log(update);
+    } else {
+      setUpdate(true);
+      console.log(update);
+    }
+  };
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`, {
       productId: cartItem.productId,
@@ -18,10 +31,10 @@ function CartitemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <input type="text" className="quantity-input" />
+            Quantity: <input type="text" className={update ? "quantity-input" : "no-display-quantity-input"} />
             <span className="quantity-label">{cartItem.quantity}</span>
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span className="update-quantity-link link-primary" onClick={showUpdateInput}>Update</span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
