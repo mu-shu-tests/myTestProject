5dd9d52861d108d77bd71cc94ea7c39473332a68
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-08-11 10:17:37 +0430

    8k important ex has been done use navigate and save the search result in url parameters

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -5,7 +5,24 @@ import LogoWhite from "../assets/images/logo-white.png";
 import MobileLoge from "../assets/images/mobile-logo-white.png";
 import SearchIcon from "../assets/images/icons/search-icon.png";
 import CartIcon from "../assets/images/icons/cart-icon.png";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 function Header({ cart }) {
+  const [searchBarText, setSearchBarText] = useState("");
+  const navigate = useNavigate();
+
+  const getSearchBarText = (event) => {
+    setSearchBarText(event.target.value);
+    navigate(`/?search=${searchBarText}`);
+  };
+  /*  const keyboardEvent = (event) => {
+    if (event.key === "Enter") {
+      setSearchBarText(event.target.value);
+    } else if (event.key === "Escape") {
+      setSearchBarText("");
+    }
+  }; */
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
@@ -22,9 +39,14 @@ function Header({ cart }) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          onChange={getSearchBarText}
+        />
 
-        <button className="search-button">
+        <button className="search-button" onClick={console.log(searchBarText)}>
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
