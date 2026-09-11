5dd7d6717a40a9049c76b47f06fd520297a5b752
Muhammad Shuaib <shuaibhemat2@gmail.com>
2026-09-11 12:02:45 +0430

    change the api port number to 10000

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:10000",
       },
       "/images": {
-        target: "http://localhost:3000",
+        target: "http://localhost:10000",
       },
     },
   },
